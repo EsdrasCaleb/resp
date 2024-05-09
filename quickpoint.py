95a1b0b3ad9ca3987b0245ddf6466e4f1c9d2f62
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2024-05-08 21:38:24 -0300

    wip

--- quickpoint.py
+++ quickpoint.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import cross_val_score, KFold
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 from db_paper import db_paper
 import csv
@@ -188,5 +190,159 @@ else:
 print("===================")
 print("===================")
 print("Checkpoint 3")
+if not os.path.isfile("checkpoint/redusida3.csv"):
+    databases = [df,df_cleaned,df_reduced,data_pca]
+    print("KNN")
 
-mlp_classifier = MLPClassifier(random_state=42)
+    print("Testando dados completos")
+    i = 0
+    nb_classifier = GaussianNB()
+    dt_classifier = DecisionTreeClassifier(random_state=42)
+    trainers = ["Decision tree","Naive Bayes"]
+    j=0
+
+    for trainer in trainers:
+        print("Trainer "+trainer)
+        finalresult = []
+
+        for database in databases:
+            result = [[], [], [], []]
+            i += 1
+            atribute = database.iloc[:, :-1]
+            classe = database.iloc[:, -1]
+            if (j == 0):
+                for k in [1,2,3,5,7,9,11]:
+                    trainer = DecisionTreeClassifier(random_state=42,max_depth=k)
+                    result[0].append(tenfold(trainer,atribute,classe))
+                    result[1].append(splited(trainer,atribute,classe,0.1))
+                    result[2].append(splited(trainer,atribute,classe,0.2))
+                    result[3].append(splited(trainer,atribute,classe,0.3))
+            else:
+                trainer = GaussianNB(priors=None, var_smoothing=1e-9)
+                result[0].append(tenfold(trainer, atribute, classe))
+                result[1].append(splited(trainer, atribute, classe, 0.1))
+                result[2].append(splited(trainer, atribute, classe, 0.2))
+                result[3].append(splited(trainer, atribute, classe, 0.3))
+            finalresult += result
+        j+=1
+        for line in finalresult:
+            print(','.join(line))
+selectors = ["10Fold","70/30"]
+plt.figure(figsize=(8, 6))
+plt.title('Melhor numero de neuronios')
+plt.xlabel('Neuronios')
+plt.ylabel('Acuracia')
+bestNeurons = {"10Fold":6,"70/30":6}
+atribute = df.iloc[:, :-1]
+classe = df.iloc[:, -1]
+for selector in selectors:
+    hiddenlayers = {}
+    testhidden = 6
+    bestguess = 6
+    downmax = False
+    downmin = False
+    while not(downmax and downmin):
+        mlp_classifier = MLPClassifier(hidden_layer_sizes=testhidden,random_state=42)
+        if(selector == "10Fold"):
+            result = tenfold(mlp_classifier, atribute, classe)
+        else:
+            result = splited(mlp_classifier, atribute, classe,0.3)
+
+        hiddenlayers[testhidden] = result
+        if(hiddenlayers[testhidden]>hiddenlayers[bestguess]):
+            bestguess = testhidden
+        elif(bestguess>testhidden):
+            downmin = True
+        elif(bestguess<testhidden):
+            downmax = True
+        if(downmin):
+            testhidden += 1
+            while testhidden in hiddenlayers.keys():
+                testhidden += 1
+        else:
+            testhidden -= 1
+            while testhidden in hiddenlayers.keys():
+                testhidden -= 1
+        print(hiddenlayers,downmin,downmax)
+    print(bestguess)
+    bestNeurons[selector] = bestguess
+    plt.plot( [float(x) for x in hiddenlayers.keys()], [float(x) for x in hiddenlayers.values()],
+              marker='o', label=selector)
+
+plt.legend()
+plt.savefig('checkpoint/hiddenlayers.png')
+plt.figure(figsize=(8, 6))
+plt.title('Melhor numero de interações')
+plt.xlabel('Interações')
+plt.ylabel('Acuracia')
+bestIntractions = {"10Fold":False,"70/30":False}
+for selector in selectors:
+    interactions = {"100":0,"1000":0,"5000":0}
+
+    for interaction in interactions.keys():
+        mlp_classifier = MLPClassifier(hidden_layer_sizes=int(bestNeurons[selector])
+                                                              ,max_iter=int(interaction),random_state=42)
+        if(selector == "10Fold"):
+            result = tenfold(mlp_classifier, atribute, classe)
+        else:
+            result = splited(mlp_classifier, atribute, classe,0.3)
+        interactions[interaction] = result
+        if not bestIntractions[selector]:
+            bestIntractions[selector] = interaction
+        elif(interactions[bestIntractions[selector]]<interactions[interaction]):
+            bestIntractions[selector] = interaction
+
+    plt.plot( [float(x) for x in interactions.keys()], [float(x) for x in interactions.values()],
+              marker='o', label=selector)
+print(bestIntractions)
+plt.legend()
+plt.savefig('checkpoint/interactions.png')
+plt.figure(figsize=(8, 6))
+plt.title('Melhor taxa de aprendizado')
+plt.xlabel('Interações')
+plt.ylabel('Acuracia')
+bestLR = {"10Fold":False,"70/30":False}
+for selector in selectors:
+    lrs = {"0.001": 0, "0.01": 0, "0.1": 0}
+
+    for lr in lrs.keys():
+        mlp_classifier = MLPClassifier(hidden_layer_sizes=int(bestNeurons[selector]),
+                                       max_iter=int(bestIntractions(selector)),
+                                       learning_rate_init=int(lr),
+                                       random_state=42)
+        if (selector == "10Fold"):
+            result = tenfold(mlp_classifier, atribute, classe)
+        else:
+            result = splited(mlp_classifier, atribute, classe, 0.3)
+        lrs[lr] = result
+        if not bestLR[selector]:
+            bestLR[selector] = lr
+        elif (lrs[bestLR[selector]] < lrs[lr]):
+            bestLR[selector] = lr
+
+    plt.plot([float(x) for x in interactions.keys()], [float(x) for x in interactions.values()],
+             marker='o', label=selector)
+print(bestLR)
+plt.legend()
+plt.savefig('checkpoint/learningrate.png')
+
+bestParameters = {"10Fold":False,"70/30":False}
+parameters = {'max_iter': [100,500,1000,2000 ], 'learning_rate_init': [0.001,0.01,0.1] ,
+'hidden_layer_sizes':np.arange(10, 100), 'random_state':[0,1,2,3,4,5,6,7,8,9]}
+for selector in selectors:
+    mlp = MLPClassifier()
+    if (selector == "10Fold"):
+        kt = KFold(n_splits=10, shuffle=True, random_state=42)
+        clf = GridSearchCV(mlp, parameters, n_jobs=-1,cv=kt)
+        clf.fit(atribute, classe)
+        print("Best Parameters 10Fold:", clf.best_params_)
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(atribute, classe, test_size=0.3, random_state=42)
+        grid_search = GridSearchCV(mlp, parameters, cv=5, n_jobs=-1, verbose=1)
+        grid_search.fit(X_train, y_train)
+        print("Best Parameters 70/30:", grid_search.best_params_)
+print("******")
+
+#fig, ax = plt.subplots()
+#boxplot = df.boxplot(ax=ax,column=['peso_fonte'])
+#plt.show()
